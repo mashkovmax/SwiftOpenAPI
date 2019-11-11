351f83df120cf75184bf5568773472c6e8e7ed4d
Max Mashkov <mashkovmax@gmail.com>
2019-11-11 22:42:41 +0200

    add AnyCodable as dependency

--- Package.swift
+++ Package.swift
@@ -13,14 +13,14 @@ let package = Package(
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
+         .package(url: "https://github.com/Flight-School/AnyCodable.git", from: "0.2.3"),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "SwiftOpenAPI",
-            dependencies: []),
+            dependencies: ["AnyCodable"]),
         .testTarget(
             name: "SwiftOpenAPITests",
             dependencies: ["SwiftOpenAPI"]),
--- Sources/SwiftOpenAPI/Schema.swift
+++ Sources/SwiftOpenAPI/Schema.swift
@@ -6,6 +6,7 @@
 //
 
 import Foundation
+import AnyCodable
 
 final class Schema: Encodable {
     struct DataType {
